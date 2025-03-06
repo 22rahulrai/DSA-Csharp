3a9a564b3b743914690a8daf8454f40c15675118
Rahul Rai <rairayrahul22@gmail.com>
2025-03-06 15:45:56 +0530

    stack linkedlist

--- Program.cs
+++ Program.cs
@@ -4,7 +4,74 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Stack_linkedlist stack = new Stack_linkedlist();
+            stack.Push(10);
+            stack.Push(20);
+            stack.Push(30);
+            stack.Push(40);
+            stack.Push(50);
+
+
+            while (true)
+            {
+                Console.WriteLine("\nChoose an option:");
+                Console.WriteLine("1. Push");
+                Console.WriteLine("2. Pop");
+                Console.WriteLine("3. Peek");
+                Console.WriteLine("4. Print Stack");
+                Console.WriteLine("5. Size");
+                Console.WriteLine("6. Check if Empty");
+                Console.WriteLine("7. Exit");
+                Console.Write("Enter your choice: ");
+
+                int choice;
+                if (!int.TryParse(Console.ReadLine(), out choice))
+                {
+                    Console.WriteLine("Invalid input! Please enter a number.");
+                    continue;
+                }
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.Write("Enter value to push: ");
+                        int value;
+                        if (int.TryParse(Console.ReadLine(), out value))
+                            stack.Push(value);
+                        else
+                            Console.WriteLine("Invalid input! Enter a valid integer.");
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Popped: " + stack.Pop());
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Top element: " + stack.Peek());
+                        break;
+
+                    case 4:
+                        stack.Print();
+                        break;
+
+                    case 5:
+                        Console.WriteLine("Size: " + stack.Size());
+                        break;
+
+                    case 6:
+                        Console.WriteLine(stack.Isempty() ? "Stack is empty" : "Stack is not empty");
+                        break;
+
+                    case 7:
+                        Console.WriteLine("Exiting...");
+                        return;
+
+                    default:
+                        Console.WriteLine("Invalid choice! Please enter a valid option.");
+                        break;
+                }
+            }
         }
+
     }
 }
---
+++ Stack-linkedlist.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA_CSharp
+{
+    public class Stack_linkedlist
+    {
+        private Node top;
+        private int size;
+
+        public Stack_linkedlist()
+        {
+            top = null;
+            size = 0;
+        }
+
+        public void Push(int item)
+        {
+            Node newnode = new Node(item);
+            newnode.next = top;
+            top = newnode;
+            size++;
+        }
+
+        public int Pop()
+        {
+            if (top == null)
+            {
+                Console.WriteLine("No element to pop");
+                return -1;
+            }
+            int pop_value = top.data;
+            top = top.next;
+            size--;
+            return pop_value;
+        }
+
+        public int Peek()
+        {
+            if (top == null)
+            {
+                Console.WriteLine("stack is empty");
+                return -1;
+            }
+
+            return top.data;
+        }
+
+        public bool Isempty()
+        {
+            return top == null;
+        }
+
+        public int Size()
+        {
+            return size;
+        }
+
+        public void Print()
+        {
+            if (top == null)
+            {
+                Console.WriteLine("stack is empty");
+                return;
+            }
+            Node curr = top;
+            while (curr != null)
+            {
+                Console.Write(curr.data + " -> ");
+                curr = curr.next;
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public class Node
+    {
+        public int data;
+        public Node next;
+
+        public Node(int data)
+        {
+            this.data = data;
+            this.next = null;
+
+        }
+    }
+}
